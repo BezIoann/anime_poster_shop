4a1a39379d8745a006122229b9c081db07e0670b
Ioann <rus.vanyok@gmail.com>
2021-09-21 22:44:30 +0300

    feat: start of the project

---
+++ src/main/java/com/animeposter/shop/AnimePosterShopApplication.java
@@ -0,0 +1,13 @@
+package com.animeposter.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimePosterShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimePosterShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animeposter/shop/AnimePosterShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeposter.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimePosterShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
